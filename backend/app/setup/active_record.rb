d2edc064022c2ba8208346037a0697de0b94a28e
shuymn <mail@shuymn.me>
2018-05-22 05:48:20 +0900

    グローバル定数を見るように変更

--- backend/app/setup/active_record.rb
+++ backend/app/setup/active_record.rb
@@ -3,4 +3,4 @@ require 'yaml'
 
 path = File.expand_path('../../config/database.yml', __dir__)
 ActiveRecord::Base.configurations = YAML.load_file(path)
-ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations[@environment])
+ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations[$env])
